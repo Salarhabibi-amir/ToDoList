9f432351a84b4603c79af3cfc36f4914f9069394
Amir Hussain habibi <108141097+Salarhabibi-amir@users.noreply.github.com>
2023-06-07 02:29:27 +0430

    Configure linters, customise the Readme file

--- src/index.js
+++ src/index.js
@@ -1,19 +1,12 @@
 import _ from 'lodash';
 import './style.css';
-import Icon from './IMG_0240.jpg';
+
 
 function component() {
     const element = document.createElement('div');
 
     // Lodash, now imported by this script
-    element.innerHTML = "some text";
-    element.classList.add('hello');
-    // Add the image to our existing div.
-    const myIcon = new Image();
-    myIcon.src = Icon;
-    myIcon.width = 200;
-    myIcon.height = 200;
-    element.appendChild(myIcon);
+    element.innerHTML = "Hello amir";
     return element;
 }
 
