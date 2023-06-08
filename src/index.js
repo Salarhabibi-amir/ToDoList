8e6675f13f3f06d3f4ae94d05aa7ba0bd069fe75
Amir Hussain habibi <108141097+Salarhabibi-amir@users.noreply.github.com>
2023-06-08 18:24:16 +0430

    create function.js file, use localstorage

--- src/index.js
+++ src/index.js
@@ -1,37 +1,7 @@
 import './style.css';
+import ToDolist from './function';
 
-const Tasks = [{
-  index: 1,
-  completed: false,
-  description: 'First task of the day',
-},
-{
-  index: 2,
-  completed: false,
-  description: 'second task of the day',
-}, {
-  index: 3,
-  completed: false,
-  description: 'Third task of the day',
-},
-];
-// display function
-
-const display = () => {
-  const todoList = document.querySelector('.todo-list');
-  Tasks.forEach((element) => {
-    const taskContainer = document.createElement('div');
-    taskContainer.innerHTML = `
-        <label class="label" for="">
-        <input type="checkbox"  id="" class="checkbox">
-        <div class="AddtodoInput">
-            <input type="text" class="input-inside-list" placeholder="${element.description}" >
-            <i class="fas fa-ellipsis-v"></i>
-        </div>
-        </label>
-        `;
-    todoList.append(taskContainer);
-  });
-};
-
-display();
+window.addEventListener('load', () => {
+    const list = new ToDolist();
+    list.display();
+});
